4d5b939733bf715544a685420d1f33ff2735abbc
lavlesh <baislavleshkumar@gmail.com>
2025-01-08 19:14:52 +0530

    new commit

---
+++ app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
@@ -0,0 +1,50 @@
+import { db } from "@/utils/dbConfig";
+import { Expenses } from "@/utils/schema";
+import { eq } from "drizzle-orm";
+import { Trash } from "lucide-react";
+import React from "react";
+import { toast } from "sonner";
+
+function ExpenseListTable({ expensesList, refreshData }) {
+  const deleteExpense = async (expense) => {
+    const result = await db
+      .delete(Expenses)
+      .where(eq(Expenses.id, expense.id))
+      .returning();
+
+    if (result) {
+      toast("Expense Deleted!");
+      refreshData();
+    }
+  };
+
+  return (
+    <div className="mt-3">
+      <h2 className="font-bold text-lg">Latest Expenses</h2>
+      <div className="grid grid-cols-4 rounded-tl-xl rounded-tr-xl bg-slate-200 p-2 mt-3">
+        <h2 className="font-bold">Name</h2>
+        <h2 className="font-bold">Amount</h2>
+        <h2 className="font-bold">Date</h2>
+        <h2 className="font-bold">Action</h2>
+      </div>
+      {expensesList.map((expense) => (
+        <div
+          key={expense.id} // Unique key for each expense item
+          className="grid grid-cols-4 bg-slate-50 rounded-bl-xl rounded-br-xl p-2"
+        >
+          <h2>{expense.name}</h2>
+          <h2>{expense.amount}</h2>
+          <h2>{expense.createdAt}</h2>
+          <h2
+            onClick={() => deleteExpense(expense)}
+            className="text-red-500 cursor-pointer"
+          >
+            Delete
+          </h2>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ExpenseListTable;
